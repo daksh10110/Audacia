9ea32881ecb8a477ed67256b19b40c6e114d7de8
daksh10110 <dakshkanotra@gmail.com>
2023-07-07 02:22:09 +0530

    All of it is junk, but it works

--- audacia/src/App.js
+++ audacia/src/App.js
@@ -4,7 +4,13 @@ import SlideShow from "./components/SlideShow";
 
 const App = () => {
   return (
-    <SlideShow />
+    <div>
+      <nav style={{ position: "fixed", top: 0, left: 0, right: 0, background: "#fff", padding: "10px", zIndex: 1 }}>
+        {/* Add your navbar content here */}
+        <h1>Navbar</h1>
+      </nav>
+      <SlideShow />
+    </div>
   );
 };
 
--- audacia/src/components/SlideShow.js
+++ audacia/src/components/SlideShow.js
@@ -8,22 +8,46 @@ SwiperCore.use([Keyboard, Mousewheel]);
 const SlideShow = () => {
   const slides = [
     {
+      key: 0,
+      content: (
+        <div>
+          <h2>Welcome to the Slide Show</h2>
+          <p>This is the introduction slide.</p>
+        </div>
+      ),
+    },
+    {
       key: 1,
-      url:
-        "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80",
+      content: (
+        <div>
+          <h2>Registration</h2>
+          {/* Include your registration form or content here */}
+          <form>
+            {/* Registration form fields */}
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+            {/* Additional registration fields */}
+          </form>
+        </div>
+      ),
     },
     {
       key: 2,
       url:
-        "https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+        "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80",
     },
     {
       key: 3,
       url:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOupnjrdRmGrfcdOJKjmmou6hppBj-Mov6UrIn2Edt-aNkM_9H33PhfJ9kOaiK7XOBEuQ&usqp=CAU",
+        "https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
     },
     {
       key: 4,
+      url:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOupnjrdRmGrfcdOJKjmmou6hppBj-Mov6UrIn2Edt-aNkM_9H33PhfJ9kOaiK7XOBEuQ&usqp=CAU",
+    },
+    {
+      key: 5,
       url:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR04HW_Ac9cyhE9xhfVuaJWaWyFoY9eTZjUOoM4rE2v8bTHSOBjtFQPMcuRy_D2kLJhJkY&usqp=CAU",
     },
@@ -40,15 +64,19 @@ const SlideShow = () => {
       >
         {slides.map((slide) => (
           <SwiperSlide key={slide.key}>
-            <img
-              src={slide.url}
-              alt="random"
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "contain",
-              }}
-            />
+            {slide.content ? (
+              slide.content
+            ) : (
+              <img
+                src={slide.url}
+                alt="random"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "contain",
+                }}
+              />
+            )}
           </SwiperSlide>
         ))}
       </Swiper>
