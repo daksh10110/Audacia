daf215a6101198b3e830438530484ba7a99ccb42
daksh10110 <dakshkanotra@gmail.com>
2023-07-07 02:17:04 +0530

    works as intended

--- audacia/src/components/SlideShow.js
+++ audacia/src/components/SlideShow.js
@@ -1,31 +1,58 @@
-import SwiperCore, { Keyboard, Mousewheel } from "swiper/core";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import "swiper/css";
+import React from "react";
+import SwiperCore, { Keyboard, Mousewheel } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
 
 SwiperCore.use([Keyboard, Mousewheel]);
 
 const SlideShow = () => {
-
   const slides = [
-    { key: 1, url: "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80" },
-    { key: 2, url: "https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" },
-    { key: 3, url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOupnjrdRmGrfcdOJKjmmou6hppBj-Mov6UrIn2Edt-aNkM_9H33PhfJ9kOaiK7XOBEuQ&usqp=CAU" },
-    { key: 4, url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR04HW_Ac9cyhE9xhfVuaJWaWyFoY9eTZjUOoM4rE2v8bTHSOBjtFQPMcuRy_D2kLJhJkY&usqp=CAU" }
+    {
+      key: 1,
+      url:
+        "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80",
+    },
+    {
+      key: 2,
+      url:
+        "https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+    },
+    {
+      key: 3,
+      url:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOupnjrdRmGrfcdOJKjmmou6hppBj-Mov6UrIn2Edt-aNkM_9H33PhfJ9kOaiK7XOBEuQ&usqp=CAU",
+    },
+    {
+      key: 4,
+      url:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR04HW_Ac9cyhE9xhfVuaJWaWyFoY9eTZjUOoM4rE2v8bTHSOBjtFQPMcuRy_D2kLJhJkY&usqp=CAU",
+    },
   ];
 
   return (
-    <Swiper
-      slidesPerView={1}
-      direction="vertical"
-      mousewheel={true}
-      keyboard={true}
-    >
-      {slides.map((slide) => (
-        <SwiperSlide key={slide.key}>
-          <img src={slide.url} alt="random" />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div style={{ height: "100vh" }}>
+      <Swiper
+        slidesPerView={1}
+        direction="vertical"
+        mousewheel={true}
+        keyboard={true}
+        style={{ height: "100%" }}
+      >
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.key}>
+            <img
+              src={slide.url}
+              alt="random"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+              }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
