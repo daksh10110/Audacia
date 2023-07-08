d7dadf25363b0e127a8e0849fcdfaecfca547b2b
daksh10110 <dakshkanotra@gmail.com>
2023-07-09 00:46:19 +0530

    Navbar is functional, need to add proper fonts

--- audacia/src/components/Navbar.js
+++ audacia/src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { SlMenu } from "react-icons/sl";
+
 const Navbar = () => {
   return (
     <nav
@@ -9,14 +11,47 @@ const Navbar = () => {
         top: 0,
         left: 0,
         right: 0,
-        background: "rgba(255, 255, 255, 0)", // Transparent background with 50% opacity
+        background: "rgba(255, 255, 255, 0.0)", // Transparent background with 50% opacity
         padding: "10px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
         zIndex: 1,
       }}
     >
-      <Link to="/">
-        <h1 style={{ color: "#000" }}>Navbar</h1> {/* Opaque text color */}
-      </Link>
+      <div>
+        <Link to="/" style={{ textDecoration: "none", color: "#000" }}>
+          <h1>Navbar</h1>
+        </Link>
+      </div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Link
+          to="/about"
+          style={{ textDecoration: "none", color: "#000", margin: "0 10px" }}
+        >
+          About
+        </Link>
+        <Link
+          to="/studio"
+          style={{ textDecoration: "none", color: "#000", margin: "0 10px" }}
+        >
+          Studio
+        </Link>
+        <Link
+          to="/lorepumispum"
+          style={{ textDecoration: "none", color: "#000", margin: "0 10px" }}
+        >
+          Lorepum Ispum
+        </Link>
+        <button style={{
+            background: "none",
+            border: "none",
+            cursor: "pointer",
+          }}
+        >
+          <SlMenu style={{ margin: "0 10px"}}/>
+        </button>
+      </div>
     </nav>
   );
 };
