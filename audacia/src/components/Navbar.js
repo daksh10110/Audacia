0102129650a4bdb563cbab87e789928d6c950766
daksh10110 <dakshkanotra@gmail.com>
2023-07-09 01:01:53 +0530

    works for now

--- audacia/src/components/Navbar.js
+++ audacia/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import { SlMenu } from "react-icons/sl";
 
 const Navbar = () => {
+  const handleMenuClick = () => {
+    // Handle menu click
+  };
+
   return (
     <nav
       style={{
@@ -11,7 +14,7 @@ const Navbar = () => {
         top: 0,
         left: 0,
         right: 0,
-        background: "rgba(255, 255, 255, 0.0)", // Transparent background with 50% opacity
+        background: "rgba(255, 255, 255, 0.0)",
         padding: "10px",
         display: "flex",
         justifyContent: "space-between",
@@ -20,36 +23,38 @@ const Navbar = () => {
       }}
     >
       <div>
-        <Link to="/" style={{ textDecoration: "none", color: "#000" }}>
-          <h1>Navbar</h1>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h1 style={{ color: "#FFFFFF" }}>Navbar</h1>
         </Link>
       </div>
       <div style={{ display: "flex", alignItems: "center" }}>
         <Link
           to="/about"
-          style={{ textDecoration: "none", color: "#000", margin: "0 10px" }}
+          style={{ textDecoration: "none", color: "#FFFFFF", margin: "0 10px" }}
         >
           About
         </Link>
         <Link
           to="/studio"
-          style={{ textDecoration: "none", color: "#000", margin: "0 10px" }}
+          style={{ textDecoration: "none", color: "#FFFFFF", margin: "0 10px" }}
         >
           Studio
         </Link>
         <Link
           to="/lorepumispum"
-          style={{ textDecoration: "none", color: "#000", margin: "0 10px" }}
+          style={{ textDecoration: "none", color: "#FFFFFF", margin: "0 10px" }}
         >
           Lorepum Ispum
         </Link>
-        <button style={{
+        <button
+          style={{
             background: "none",
             border: "none",
             cursor: "pointer",
           }}
+          onClick={handleMenuClick}
         >
-          <SlMenu style={{ margin: "0 10px"}}/>
+          <SlMenu style={{ color: "#FFFFFF", margin: "0 10px" }} />
         </button>
       </div>
     </nav>
--- audacia/src/components/SlideShow.js
+++ audacia/src/components/SlideShow.js
@@ -3,58 +3,43 @@ import SwiperCore, { Keyboard, Mousewheel } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 
+import slide_1 from '../images/slide_1.png'
+import slide_2 from '../images/slide_2.png'
+
 SwiperCore.use([Keyboard, Mousewheel]);
 
 const SlideShow = () => {
   const slides = [
     {
       key: 0,
-      content: (
-        <div style={{ marginTop: "10vh" }}>
-          <h2>Welcome to the Slide Show</h2>
-          <p>This is the introduction slide.</p>
-        </div>
-      ),
+      url:
+        slide_1,
     },
     {
       key: 1,
-      content: (
-        <div style={{ marginTop: "10vh" }}>
-          <h2>Registration</h2>
-          {/* Include your registration form or content here */}
-          <form>
-            {/* Registration form fields */}
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" />
-            {/* Additional registration fields */}
-          </form>
-        </div>
-      ),
+      url:
+        slide_2,
     },
     {
       key: 2,
       url:
-        "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80",
-    },
-    {
-      key: 3,
-      url:
-        "https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
-    },
-    {
-      key: 4,
-      url:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOupnjrdRmGrfcdOJKjmmou6hppBj-Mov6UrIn2Edt-aNkM_9H33PhfJ9kOaiK7XOBEuQ&usqp=CAU",
     },
     {
-      key: 5,
+      key: 3,
       url:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR04HW_Ac9cyhE9xhfVuaJWaWyFoY9eTZjUOoM4rE2v8bTHSOBjtFQPMcuRy_D2kLJhJkY&usqp=CAU",
     },
   ];
 
   return (
-    <div style={{ height: "100vh" }}>
+    <div
+      style={{
+        height: "100vh",
+        position: "relative",
+        overflow: "hidden",
+      }}
+    >
       <Swiper
         slidesPerView={1}
         direction="vertical"
@@ -64,24 +49,26 @@ const SlideShow = () => {
       >
         {slides.map((slide) => (
           <SwiperSlide key={slide.key}>
-            {slide.content ? (
-              slide.content
-            ) : (
-              <img
-                src={slide.url}
-                alt="random"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "contain",
-                }}
-              />
-            )}
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundImage: `url(${slide.url})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                filter: `blur(${slide.blur}px)`,
+                zIndex: -1,
+              }}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
     </div>
   );
 };
+
 
 export default SlideShow;
